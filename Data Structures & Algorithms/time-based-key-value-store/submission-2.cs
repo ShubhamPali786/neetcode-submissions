97ce591af0483108426df233f7ecf3a74c8e80d3
ShubhamPali786 <38189510+ShubhamPali786@users.noreply.github.com>
2026-06-14 18:36:22 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cs
@@ -0,0 +1,33 @@
+public class TimeMap {
+    private Dictionary<string, List<(int timestamp, string value)>> store;
+
+    public TimeMap() {
+        store = new Dictionary<string, List<(int timestamp, string value)>>();
+    }
+
+    public void Set(string key, string value, int timestamp) {
+        if(!store.ContainsKey(key)){
+            store[key] = new List<(int timestamp, string value)>();
+        }
+        store[key].Add((timestamp,value));
+    }
+
+    public string Get(string key, int timestamp) {
+        if(!store.ContainsKey(key))
+            return "";
+        var entries = store[key];
+        int right = entries.Count - 1;
+        int left = 0;
+        var result = "";
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(entries[mid].timestamp <= timestamp){
+                result = entries[mid].value;
+                left = mid + 1;
+            }else{
+                right = mid -1;
+            }
+        }
+        return result;
+    }
+}
